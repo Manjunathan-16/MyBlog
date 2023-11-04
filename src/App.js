87c2b0cf14b3bcfa2d87ad27f229c566299a5862
Manjunathan-16 <manjunathan.16.1998@gmail.com>
2023-11-04 17:10:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+import React from "react";
+import Blog from "./Component/Blog";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Link to="/" />
+        <Routes>
+          <Route path="/" element={<Blog />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Component/Blog.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+export default class Blog extends Component {
+  render() {
+    return (
+      <>
+        <div className="App">
+          Hello everyone, I'm Manjunathan, and I'd like to share a bit about my
+          educational and professional journey with you. I completed a total of
+          16 years of education, including my undergraduate studies, in the
+          vibrant city of Bangalore. My undergraduate degree was in Electrical
+          and Electronics Engineering, and I pursued it at Acharya Institute of
+          Technology. During my final year of college, I received an exciting
+          opportunity to join Infosys, one of the leading IT companies in India.
+          However, due to the unforeseen challenges posed by the COVID-19
+          pandemic, my college completion was delayed by a couple of months, and
+          this, in turn, affected my joining date. I officially became a part of
+          the Infosys family on May 3rd, 2021. My training at Infosys was unique
+          as it took place in a work-from-home (WFH) setting. During this
+          period, I gained valuable skills in Java, Spring Boot, and React.js,
+          which are integral in the world of software development. My training
+          journey came to an end on September 8th, 2021, and I was eager to
+          embark on the next phase of my career. Shortly after completing my
+          training, I was placed in a Cisco project where I am responsible for
+          managing two significant projects: "Simple Upgrade" and "Prime
+          Collaboration Deployment." In this role, I primarily work with Java
+          and JavaScript to develop and maintain these projects. I'm excited
+          about the opportunities that lie ahead and the contributions I can
+          make in the field of software development. Thank you for taking the
+          time to learn a little bit about my background and experiences.
+        </div>
+      </>
+    );
+  }
+}
